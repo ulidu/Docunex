7776e0e4d03b491a104d442db0ee31997b48c1cd
IT18126198_Ulidu <it18126198@my.sliit.lk>
2021-09-14 02:04:54 +0530

    Project Preparation

--- login.php
+++ login.php
@@ -1,37 +1,55 @@
-<!DOCTYPE html>
+<?php
 
+include 'dist/include/db.php';
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
 <html lang="en" class="light">
-    <!-- BEGIN: Head -->
-    <head>
-        <meta charset="utf-8">
-        <link href="dist/images/logo.svg" rel="shortcut icon">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Rubick admin is super flexible, powerful, clean & modern responsive tailwind admin template with unlimited possibilities.">
-        <meta name="keywords" content="admin template, Rubick Admin Template, dashboard template, flat admin template, responsive admin template, web app">
-        <meta name="author" content="LEFT4CODE">
-        <title>Login - Rubick - Tailwind HTML Admin Template</title>
-        <!-- BEGIN: CSS Assets-->
-        <link rel="stylesheet" href="dist/css/app.css" />
-        <!-- END: CSS Assets-->
-    </head>
-    <!-- END: Head -->
+<!-- BEGIN: Head -->
+<head>
+    <meta charset="utf-8">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="dist/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="dist/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="dist/favicon/favicon-16x16.png">
+    <link rel="manifest" href="dist/favicon/site.webmanifest">
+    <link rel="mask-icon" href="dist/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="theme-color" content="#ffffff">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Docunex - Document Management System">
+    <meta name="author" content="Docunex">
+
+    <title>Docunex</title>
+
+    <!-- BEGIN: CSS Assets-->
+    <link rel="stylesheet" href="dist/css/app.css"/>
+    <!-- END: CSS Assets-->
+
+</head>
+<!-- END: Head -->
     <body class="login">
         <div class="container sm:px-10">
             <div class="block xl:grid grid-cols-2 gap-4">
                 <!-- BEGIN: Login Info -->
                 <div class="hidden xl:flex flex-col min-h-screen">
-                    <a href="" class="-intro-x flex items-center pt-5">
-                        <img alt="Rubick Tailwind HTML Admin Template" class="w-6" src="dist/images/logo.svg">
-                        <span class="text-white text-lg ml-3"> Ru<span class="font-medium">bick</span> </span>
+                    <a href="" class="intro-x flex items-center pl-5 pt-4">
+                        <img class="w-8" src="dist/images/logo.png">
+                        <span class="hidden xl:block text-white text-lg ml-3" style="letter-spacing: 0.05rem;">
+                Docunex</span>
                     </a>
                     <div class="my-auto">
-                        <img alt="Rubick Tailwind HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="dist/images/illustration.svg">
+                        <img alt="Rubick Tailwind HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="dist/images/login.png">
                         <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">
                             A few more clicks to 
                             <br>
                             sign in to your account.
                         </div>
-                        <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-gray-500">Manage all your e-commerce accounts in one place</div>
+                        <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-gray-500">Manage all your documents in one place</div>
                     </div>
                 </div>
                 <!-- END: Login Info -->
@@ -54,8 +72,8 @@
                             <a href="">Forgot Password?</a> 
                         </div>
                         <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
-                            <button class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Login</button>
-                            <button class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Sign up</button>
+                            <a href="index.php" class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Login</a>
+                            <a href="register.php" class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Sign up</a>
                         </div>
                         <div class="intro-x mt-10 xl:mt-24 text-gray-700 dark:text-gray-600 text-center xl:text-left">
                             By signin up, you agree to our 
@@ -67,12 +85,7 @@
                 <!-- END: Login Form -->
             </div>
         </div>
-        <!-- BEGIN: Dark Mode Switcher-->
-        <div data-url="login-dark-login.html" class="dark-mode-switcher cursor-pointer shadow-md fixed bottom-0 right-0 box dark:bg-dark-2 border rounded-full w-40 h-12 flex items-center justify-center z-50 mb-10 mr-10">
-            <div class="mr-4 text-gray-700 dark:text-gray-300">Dark Mode</div>
-            <div class="dark-mode-switcher__toggle border"></div>
-        </div>
-        <!-- END: Dark Mode Switcher-->
+
         <!-- BEGIN: JS Assets-->
         <script src="dist/js/app.js"></script>
         <!-- END: JS Assets-->
